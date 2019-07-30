e9f696ceb40831a635bc24b649feeccc40495600
Warren Yao <warrenyca905@gmail.com>
2019-07-30 04:02:12 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -50,5 +50,9 @@ ORDER BY pledges.id;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
-"Write your SQL query Here"
+"SELECT category, SUM(pledges.amount)
+FROM projects
+LEFT JOIN pledges
+on pledges.project_id = projects.id
+WHERE category = 'books'"
 end
