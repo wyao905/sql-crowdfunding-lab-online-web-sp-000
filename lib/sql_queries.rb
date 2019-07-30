b55d5508456eb50bb48251775fe7dada68c3eb57
Warren Yao <warrenyca905@gmail.com>
2019-07-30 03:55:37 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -45,7 +45,7 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
 FROM projects
 LEFT JOIN pledges
 ON pledges.project_id = projects.id
-GROUP BY SUM(pledges.amount)
+GROUP BY pledges.amount
 HAVING category = 'music';"
 end
 
