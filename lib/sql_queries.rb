6489330e929d19918a3303bc1b00318c9128cde4
Warren Yao <warrenyca905@gmail.com>
2019-07-30 04:02:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -54,5 +54,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
 FROM projects
 LEFT JOIN pledges
 on pledges.project_id = projects.id
-WHERE category = 'books'"
+WHERE category = 'books'
+GROUP BY title;"
 end
