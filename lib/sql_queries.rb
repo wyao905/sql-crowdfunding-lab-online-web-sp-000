2dbc428df32de157d61ee316f7ae8d3277a7e237
Warren Yao <warrenyca905@gmail.com>
2019-07-30 03:55:02 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -41,12 +41,12 @@ ORDER BY SUM(pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"SELECT name, SUM(pledges.amount)
+"SELECT category, SUM(pledges.amount)
 FROM projects
 LEFT JOIN pledges
 ON pledges.project_id = projects.id
 GROUP BY SUM(pledges.amount)
-HAVING projects.category = music;"
+HAVING category = music;"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
