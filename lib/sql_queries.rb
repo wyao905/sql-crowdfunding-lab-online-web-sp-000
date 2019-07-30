e349e3951ebf175fd6fea62b3f8fc26a09d201fe
Warren Yao <warrenyca905@gmail.com>
2019-07-30 03:34:57 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -35,9 +35,9 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 "SELECT name, SUM(pledges.amount)
 FROM users
 LEFT JOIN pledges
-ON pledges.user_id = 19
+ON pledges.user_id = users.id
 GROUP BY users.name
-ORDER BY pledges.amount, users.name;"
+ORDER BY SUM(pledges.amount), users.name;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
