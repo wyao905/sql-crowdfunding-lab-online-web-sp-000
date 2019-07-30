e314ef600c2ca7af1e4a1dfd6f12c8b6a414748c
Warren Yao <warrenyca905@gmail.com>
2019-07-30 03:30:17 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -36,7 +36,7 @@ def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_th
 FROM users
 LEFT JOIN pledges
 ON pledges.user_id = users.id
-GROUP BY users.name
+GROUP BY pledges.user_id
 ORDER BY pledges.amount, users.name;"
 end
 
